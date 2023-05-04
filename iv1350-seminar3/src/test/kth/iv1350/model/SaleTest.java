28af5399f9c708e98dc4c3c19e02a4b4cd67bd79
Roy Evert Andrej Dolonius <124092984+Gigglinglizard@users.noreply.github.com>
2023-05-04 20:30:26 +0200

    cleaned up test code

--- iv1350-seminar3/src/test/kth/iv1350/model/SaleTest.java
+++ iv1350-seminar3/src/test/kth/iv1350/model/SaleTest.java
@@ -5,9 +5,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import se.kth.iv1350.integration.*;
 import se.kth.iv1350.model.*;
-import se.kth.iv1350.view.*;
-import se.kth.iv1350.controller.*;
-import java.time.*;
 import java.util.*;
 
 
@@ -15,22 +12,8 @@ public class SaleTest {
     private Item item1;
     private Item item2; 
     private Sale sale;
-    private int itemIdentifier;
     private ItemDescription itemDescription1;
     private ItemDescription itemDescription2;
-    private double storeQuantity;
-    private double tempQuantity;
-    private String name;
-    private double price;
-    private double VAT;
-    private double addedVAT;
-    private double runningTotal;
-    private LocalDateTime dateAndTime;
-    private SaleDTO testSale;
-    private controller contr;
-    private ExternalInventory extInv;
-    private ExternalAccounting extAcc;
-    private Printer printer; 
 
     @Test
     public void addItem()  {
@@ -56,10 +39,7 @@ public class SaleTest {
         itemDescription2 = new ItemDescription("Canned Beans", 55.0, 0.12);
         item1 = new Item(2, itemDescription1, 500.0);
         item2 = new Item(4, itemDescription2, 250.0);
-        List <Item> items = sale.getItems();
-        testSale = new SaleDTO(dateAndTime, runningTotal, addedVAT, items);
 
-        
         sale.calculateCost(item1, 1.0, sale);
         sale.calculateCost(item2, 1.0, sale); 
 
